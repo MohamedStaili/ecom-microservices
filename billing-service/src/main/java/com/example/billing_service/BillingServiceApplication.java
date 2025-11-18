ba4e7480d8c3c4962fcc189ac9719f73fb45bb5b
MohamedStaili <5254mohamed@gmail.com>
2025-11-18 12:26:22 +0100

    add vault key value

--- billing-service/src/main/java/com/example/billing_service/BillingServiceApplication.java
+++ billing-service/src/main/java/com/example/billing_service/BillingServiceApplication.java
@@ -1,13 +1,31 @@
 package com.example.billing_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.vault.core.VaultTemplate;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class BillingServiceApplication {
+	private VaultTemplate vaultTemplate;
 
-	public static void main(String[] args) {
+    public BillingServiceApplication(VaultTemplate vaultTemplate) {
+        this.vaultTemplate = vaultTemplate;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(BillingServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init() {
+		return args -> {
+			vaultTemplate.opsForVersionedKeyValue("secret")
+					.put("mySecret", Map.of("p1","654321"));
+		};
+
+	}
 }
--- billing-service/src/main/java/com/example/billing_service/ConsulConfigRestController.java
+++ billing-service/src/main/java/com/example/billing_service/ConsulConfigRestController.java
@@ -5,16 +5,19 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RefreshScope
+import java.util.Map;
+
+//@RefreshScope pas besoin lorsq'on utilise ConfigrationProperties
 @RestController
 public class ConsulConfigRestController {
     private MyConsulConfig myConsulConfig;
-    public ConsulConfigRestController(MyConsulConfig myConsulConfig) {
+    private MyVaultConfig myVaultConfig;
+    public ConsulConfigRestController(MyConsulConfig myConsulConfig, MyVaultConfig myVaultConfig) {
         this.myConsulConfig = myConsulConfig;
+        this.myVaultConfig = myVaultConfig;
     }
-
     @GetMapping("/myConfig")
-    public MyConsulConfig myConfig(){
-        return myConsulConfig;
+    public Map<String,Object> myConfig(){
+        return Map.of("config", myConsulConfig, "vault", myVaultConfig);
     }
 }
